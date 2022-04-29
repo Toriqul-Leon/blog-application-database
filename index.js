1e52959bd4f7207caff287789154d13a19747060
Toriqul-Leon <yeitoriqul@gmail.com>
2022-04-29 14:16:43 +0600

    Finished

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 var cors = require("cors");
-const res = require("express/lib/response");
+
 const port = process.env.PORT || 5000;
 
 // !Middleware
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server Running");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmtuj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
